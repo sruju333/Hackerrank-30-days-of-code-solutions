b34e8e9f817cc442ae0887b8623c69880cc6a6f0
Srujana N <70315274+sruju333@users.noreply.github.com>
2021-04-28 19:42:32 +0530

    Create Day11_solutions.c

---
+++ Day11_solutions.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int sum, arr[6][6];
+    for(int i=0; i<6; i++)
+    {
+        for(int j=0; j<6; j++)
+            scanf("%d", &arr[i][j]);
+    }      
+    int max=arr[0][0]+arr[0][1]+arr[0][2]+arr[1][1]+arr[2][0]+arr[2][1]+arr[2][2];
+    
+    for(int i=0; i<=3; i++)
+    {
+        for(int j=0; j<=3; j++)
+        {
+            sum=0;
+            sum=arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+            max=sum>max?sum:max;
+        }
+    }   
+    printf("%d", max);
+    
+    return 0;
+}
