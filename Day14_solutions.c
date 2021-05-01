7c99d12e66b8ce995345aa56f139ddb16ba72a84
Srujana N <70315274+sruju333@users.noreply.github.com>
2021-05-01 20:01:45 +0530

    Create Day14_solutions.c

---
+++ Day14_solutions.c
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+
+	// Add your code here
+    public Difference(int[] a){
+        elements = a;
+    }
+    
+    public void computeDifference(){
+        maximumDifference = 0;
+        int posnum = 0;
+        for(int i=0; i<elements.length; i++){
+            for(int j=0; j<elements.length; j++){
+                posnum = Math.abs(elements[i]-elements[j]);
+                 if (posnum>maximumDifference){
+                     maximumDifference = posnum;
+                 }
+            }
+        }
+    }
+
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
